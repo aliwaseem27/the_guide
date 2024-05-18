d471fc792f651db7793b174f248e684421f65fb5
aliwaseem27 <ali.waseem27@gmail.com>
2024-05-18 21:32:48 +0300

    add: CustomerDetailsScreen

--- lib/presentation/customers/screens/customer_details/customer_details_screen.dart
+++ lib/presentation/customers/screens/customer_details/customer_details_screen.dart
@@ -7,6 +7,107 @@ class CustomerDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Customer Details"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              CustomerInfoTile(
+                leadingIcon: Icons.person,
+                overlineText: "Customer Name",
+                mainText: "Ali Waseem Mohammed",
+                onTap: () {},
+              ),
+              SizedBox(height: 8),
+              CustomerInfoTile(
+                leadingIcon: Icons.phone,
+                overlineText: "Phone Number",
+                mainText: "078 123 4567",
+                onTap: () {},
+              ),
+              SizedBox(height: 8),
+              CustomerInfoTile(
+                leadingIcon: Icons.email,
+                overlineText: "Email Address",
+                mainText: "ali.waseem27@gmail.com",
+                onTap: () {},
+              ),
+              SizedBox(height: 8),
+              CustomerInfoTile(
+                leadingIcon: Icons.location_on,
+                overlineText: "Location",
+                mainText: "Karbala, Saif Saad",
+                onTap: () {},
+              ),
+              SizedBox(height: 8),
+              CustomerInfoTile(
+                leadingIcon: Icons.date_range,
+                overlineText: "Date Added",
+                mainText: "24/02/2024",
+                onTap: () {},
+              ),
+              SizedBox(height: 8),
+              CustomerInfoTile(
+                leadingIcon: Icons.tag,
+                overlineText: "Tags",
+                mainText: "Supermarket, Medium Importance",
+                onTap: () {},
+              ),
+              SizedBox(height: 8),
+              CustomerInfoTile(
+                leadingIcon: Icons.info,
+                overlineText: "Additional Info",
+                mainText: "This is multi line text info about notes can be added about the customer",
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomerInfoTile extends StatelessWidget {
+  const CustomerInfoTile({
+    super.key,
+    required this.leadingIcon,
+    required this.overlineText,
+    required this.mainText,
+    this.onTap,
+  });
+
+  final IconData leadingIcon;
+  final String overlineText;
+  final String mainText;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(leadingIcon),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            overlineText,
+            style: TextStyle(fontSize: 12),
+          ),
+          Text(mainText),
+        ],
+      ),
+      trailing: InkWell(
+        child: SizedBox(
+          child: Icon(Icons.edit),
+          width: 56,
+          height: 56,
+        ),
+        onTap: onTap,
+      ),
+    );
   }
 }
--- lib/presentation/customers/screens/customer_list/customer_list_screen.dart
+++ lib/presentation/customers/screens/customer_list/customer_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../core/routes/app_router.dart';
 
 @RoutePage()
 class CustomerListScreen extends StatelessWidget {
@@ -7,8 +10,32 @@ class CustomerListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Customer List Screen"),
+    return Padding(
+      padding: EdgeInsets.all(24.0),
+      child: ListView.separated(
+        itemCount: 14,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("Ali Waseem Mohammed"),
+            subtitle: Text("Saif Saad" + ", " + "Medium"),
+            leading: CircleAvatar(
+              backgroundColor: Colors.teal,
+              child: Text(
+                "AW",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            onTap: () => context.router.push(const CustomerDetailsRoute()),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider();
+        },
+      ),
     );
   }
 }
